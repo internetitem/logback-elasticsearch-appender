6dc9c2da74e6f480d529c930ee4adab92ac11f76
avi <anatoly.ivanov@gmail.com>
2016-02-11 10:02:11 -0800

    Adding support for messages in raw JSON format.

---
+++ src/main/java/com/internetitem/logback/elasticsearch/ClassicElasticsearchJsonRawMessagePublisher.java
@@ -0,0 +1,33 @@
+package com.internetitem.logback.elasticsearch;
+
+import java.io.IOException;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Context;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.internetitem.logback.elasticsearch.config.ElasticsearchProperties;
+import com.internetitem.logback.elasticsearch.config.HttpRequestHeaders;
+import com.internetitem.logback.elasticsearch.config.Property;
+import com.internetitem.logback.elasticsearch.config.Settings;
+import com.internetitem.logback.elasticsearch.util.AbstractPropertyAndEncoder;
+import com.internetitem.logback.elasticsearch.util.ClassicPropertyAndEncoder;
+import com.internetitem.logback.elasticsearch.util.ErrorReporter;
+
+public class ClassicElasticsearchJsonRawMessagePublisher extends AbstractElasticsearchPublisher<ILoggingEvent> {
+
+	public ClassicElasticsearchJsonRawMessagePublisher(Context context, ErrorReporter errorReporter, Settings settings, ElasticsearchProperties properties, HttpRequestHeaders headers) throws IOException {
+		super(context, errorReporter, settings, properties, headers);
+	}
+
+	@Override
+	protected AbstractPropertyAndEncoder<ILoggingEvent> buildPropertyAndEncoder(Context context, Property property) {
+		return new ClassicPropertyAndEncoder(property, context);
+	}
+
+	@Override
+	protected void serializeCommonFields(JsonGenerator gen, ILoggingEvent event) throws IOException {
+		gen.writeObjectField("@timestamp", getTimestamp(event.getTimeStamp()));
+                gen.writeFieldName("message");
+                gen.writeRawValue(event.getFormattedMessage());
+	}
+}
