fca650b088acace83008353f654ad2d1a431e1c9
Jeff Martin <martinjeffa@johndeere.com>
2016-09-23 09:54:06 -0500

    Address PR comment, ensure Java 1.7 build so no one else gets surprised

--- src/main/java/com/internetitem/logback/elasticsearch/config/BasicAuthentication.java
+++ src/main/java/com/internetitem/logback/elasticsearch/config/BasicAuthentication.java
@@ -1,13 +1,13 @@
 package com.internetitem.logback.elasticsearch.config;
 
+import javax.xml.bind.DatatypeConverter;
 import java.net.HttpURLConnection;
-import java.util.Base64;
 
-public class BasicAuthAuthentication implements Authentication {
+public class BasicAuthentication implements Authentication {
     public void addAuth(HttpURLConnection urlConnection, String body) {
         String userInfo = urlConnection.getURL().getUserInfo();
         if (userInfo != null) {
-            String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userInfo.getBytes()));
+            String basicAuth = "Basic " + DatatypeConverter.printBase64Binary(userInfo.getBytes());
             urlConnection.setRequestProperty("Authorization", basicAuth);
         }
     }
