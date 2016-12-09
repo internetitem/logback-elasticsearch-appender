98d049dc6e4a78f367b2eb63db28304009536aee
Sridharan Kuppa <sridharan.kuppa@gmail.com>
2016-12-09 10:29:34 -0800

    Replace the java.xml.bind (jaxb) package usages with JDK built-in or custom util implementation. It helps to use this library with with JDK 7 and Android project.

--- src/main/java/com/internetitem/logback/elasticsearch/config/BasicAuthentication.java
+++ src/main/java/com/internetitem/logback/elasticsearch/config/BasicAuthentication.java
@@ -1,13 +1,14 @@
 package com.internetitem.logback.elasticsearch.config;
 
-import javax.xml.bind.DatatypeConverter;
+import com.internetitem.logback.elasticsearch.util.Base64;
+
 import java.net.HttpURLConnection;
 
 public class BasicAuthentication implements Authentication {
     public void addAuth(HttpURLConnection urlConnection, String body) {
         String userInfo = urlConnection.getURL().getUserInfo();
         if (userInfo != null) {
-            String basicAuth = "Basic " + DatatypeConverter.printBase64Binary(userInfo.getBytes());
+            String basicAuth = "Basic " + Base64.encode(userInfo.getBytes());
             urlConnection.setRequestProperty("Authorization", basicAuth);
         }
     }
